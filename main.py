89e828f00df08b625b9a202d5872f4f5282e6d47
Felix Kuhlmann <felix-kuhlmann@gmx.de>
2024-02-28 21:29:02 +0100

    Added update func for statemachine
    This could enable a remote start feature or something
    
    Signed-off-by: Felix Kuhlmann <felix-kuhlmann@gmx.de>

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 import pymongo
 from datetime import datetime, timedelta
+from pydantic import BaseModel
 
 USER = "user"
 PASS = "pass"
@@ -8,6 +9,13 @@ HOST = "localhost"
 PORT = "27017"
 
 CONNECTION_STRING = "mongodb://" + USER + ":" + PASS + "@" + HOST + ":" + PORT + "/" 
+
+# Set update value class
+
+class ValueUpdateRecord(BaseModel):
+    component: str
+    parameter: str
+    newValue: str | int | float
 
 # Function declarations
 def connect_to_mongodb():
@@ -23,25 +31,25 @@ def select_col(db, colName):
 app = FastAPI()
 
 # FastAPI methods
-@app.get("/data/{component}/{value}")
-async def data_return(component, value, fromWhen : str = ""):
+@app.get("/data/{component}/{parameter}")
+async def data_return(component, parameter, fromWhen : str = ""):
     databaseClient = connect_to_mongodb()
     
     # Select the appropriate component database based on the variable in the path
     componentDB = select_db(databaseClient, component)
     
     try:
-        componentDB.validate_collection(value)
+        componentDB.validate_collection(parameter)
     except pymongo.errors.OperationFailure:
-        return { "message": "ERROR: The specified component and/or the value do not exist.", "error": 1 }
+        return { "message": "ERROR: The specified component and/or the parameter do not exist.", "error": 1 }
 
-    # Select the approriate value collection according to the variable in the path
-    valueCol = select_col(componentDB, value)
+    # Select the approriate parameter collection according to the variable in the path
+    parameterCol = select_col(componentDB, parameter)
 
     if (fromWhen == ""):
         # No time specified, return the newest element
         # Filter for the newest document, exclude id because not processable
-        newestDocument = valueCol.find({},{ "_id": 0}).sort("timestamp", -1)[0]
+        newestDocument = parameterCol.find({},{ "_id": 0}).sort("timestamp", -1)[0]
 
         # Return the newest document as a result
         return newestDocument
@@ -52,7 +60,7 @@ async def data_return(component, value, fromWhen : str = ""):
         timestampNHoursAgo = (datetime.now() - timedelta(hours = numberOfHours)).isoformat()
 
         # Create an array of documents that are newer than the timestamp, exclude id again
-        documentsNewerThanTimestamp = valueCol.find({ "timestamp" : { "$gt": timestampNHoursAgo}}, { "_id": 0})
+        documentsNewerThanTimestamp = parameterCol.find({ "timestamp" : { "$gt": timestampNHoursAgo}}, { "_id": 0})
         
         # Create a dictionary that contains the results
         resultDictionary = { "results": []}
@@ -69,7 +77,7 @@ async def data_return(component, value, fromWhen : str = ""):
         timestampNMinutesAgo = (datetime.now() - timedelta(minutes = numberOfMinutes)).isoformat()
 
         # Create an array of documents that are newer than the timestamp, exclude id again
-        documentsNewerThanTimestamp = valueCol.find({ "timestamp" : { "$gt": timestampNMinutesAgo}}, { "_id": 0})
+        documentsNewerThanTimestamp = parameterCol.find({ "timestamp" : { "$gt": timestampNMinutesAgo}}, { "_id": 0})
         
         # Create a dictionary that contains the results
         resultDictionary = { "results": []}
@@ -82,3 +90,23 @@ async def data_return(component, value, fromWhen : str = ""):
     else:
         # Parameter error
         return { "message": "ERROR: The query parameters provided are wrong/incomplete.", "error": 2 }
+    
+@app.post("/update_value")
+async def data_insert(vUR: ValueUpdateRecord):
+    # Enable remote update of the state machine of the main controller
+    if (vUR.component == "drivecontroller" and vUR.parameter == "statemachine_state"):
+        # Validate that the state is valid (0...3)
+        if (vUR.newValue < 0 or vUR.newValue > 3):
+            return
+
+        # Connect to mongoDB and change the value accordingly
+        databaseClient = connect_to_mongodb()
+        drivecontrollerDB = select_db(databaseClient, "drivecontroller")
+        statemachineCol = select_col(drivecontrollerDB, "statemachine_state")
+
+        # Generate timestamp
+        parameterTimestamp = datetime.now().isoformat()
+        newState = {"displayName": "Statemachine state","category": "Drivecontroller","value": vUR.newValue,"unit": "","timestamp": parameterTimestamp}
+
+        statemachineCol.insert_one(newState)
+
